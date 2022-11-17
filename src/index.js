5a1f51052b8cbc188a4e3b1d9b252adbd18ba3d6
Ben Sales <adebsa2401@gmail.com>
2022-11-17 20:00:12 +0100

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -12,19 +12,19 @@ document.querySelector('form').addEventListener('submit', async (event) => {
   const score = formData.get('score');
 
   const scoreTab = document.querySelector('#scores-tab');
-  
-  const response = await fetch(process.env.BASE_URL + '/games/' + process.env.GAME_ID + '/scores/', {
+
+  await fetch(`${process.env.BASE_URL}/games/${process.env.GAME_ID}/scores/`, {
     method: 'POST',
     body: JSON.stringify({ user, score }),
     mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
-      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    }
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    },
   });
-  
+
   if (scoreTab.childElementCount === 0) {
     scoreTab.innerHTML = '';
     scoreTab.classList.remove('empty-scores-tab');
--- src/renders/refreshScoresTab.js
+++ src/renders/refreshScoresTab.js
@@ -1,19 +1,21 @@
+import renderScore from './scoreRow.js';
+
 export default async () => {
   const scoreTab = document.querySelector('#scores-tab');
 
-  const response = await fetch(process.env.BASE_URL + '/games/' + process.env.GAME_ID + '/scores/', {
+  const response = await fetch(`${process.env.BASE_URL}/games/${process.env.GAME_ID}/scores/`, {
     mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
-      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    }
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    },
   });
-  
+
   scoreTab.classList.remove('empty-scores-tab');
   scoreTab.innerHTML = '';
 
-  const scores = JSON.parse(await response.json())['result'];
+  const scores = JSON.parse(await response.json()).result;
   scores.forEach((score) => scoreTab.append(renderScore(score)));
-}
+};
